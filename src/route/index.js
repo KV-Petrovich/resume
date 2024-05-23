83aa5750dcbe38a460a03b6b7b2cc656ca0373f4
KV-Petrovich <petrovichnet.ukr.net>
2024-05-23 13:57:47 +0300

    exercize #1

--- src/route/index.js
+++ src/route/index.js
@@ -40,7 +40,9 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('index', {
+    layout: 'index',
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
@@ -3388,5 +3390,6 @@ router.get('/bootstrapcss', function (req, res) {
 })
 
 // ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
